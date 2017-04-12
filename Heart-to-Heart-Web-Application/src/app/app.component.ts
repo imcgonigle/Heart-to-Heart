85e97426a244b23b5ef011e9f0d2292926b047ab
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-12 14:22:51 -0600

    get firebase database working

--- Heart-to-Heart-Web-Application/src/app/app.component.ts
+++ Heart-to-Heart-Web-Application/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 // AngularFire2 
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { AngularFire, FirebaseListObservable} from 'angularfire2';
+
 
 @Component({
   selector: 'app-root',
@@ -11,12 +12,12 @@ export class AppComponent {
   title = 'Heart to Heart';
   items: FirebaseListObservable<any[]>;
 
-  constructor(public af: AngularFire ){
+  constructor(public af: AngularFire){
     this.items = af.database.list('/items');
   }
 
   login() {
-    this.af.auth.login();
+    this.af.auth.login()
   }
 
   logout() {
