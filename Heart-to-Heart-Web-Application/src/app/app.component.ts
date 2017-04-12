e172c1c729ec6d84bff3b1ff148e897915c51e95
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-12 13:36:31 -0600

    create login and logout methods

--- Heart-to-Heart-Web-Application/src/app/app.component.ts
+++ Heart-to-Heart-Web-Application/src/app/app.component.ts
@@ -9,8 +9,17 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 })
 export class AppComponent {
   title = 'Heart to Heart';
-  items: FirebaseListObservable<any>
-  constructor(af: AngularFire ){
+  items: FirebaseListObservable<any[]>;
+
+  constructor(public af: AngularFire ){
     this.items = af.database.list('/items');
+  }
+
+  login() {
+    this.af.auth.login();
+  }
+
+  logout() {
+    this.af.auth.logout();
   }
 }
