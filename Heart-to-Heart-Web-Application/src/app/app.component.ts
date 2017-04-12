ff64635ba30a13dda3cfe36496505b5e65a84d40
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-12 13:06:36 -0600

    Inject AngularFire

--- Heart-to-Heart-Web-Application/src/app/app.component.ts
+++ Heart-to-Heart-Web-Application/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// AngularFire2 
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Heart to Heart';
+
+  constructor(af: AngularFire ){}
 }
