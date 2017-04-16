bd299401d9998115da8cd889ad895220229041f5
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-15 18:15:42 -0600

    setup really basic component items

--- heart-to-heart-native/app/pages/main/main.component.ts
+++ heart-to-heart-native/app/pages/main/main.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ns-main',
-  templateUrl: 'pages/main/main.html',
-  styleUrls: ["pages/main/main.css", ]
+  templateUrl: './pages/main/main.html',
+  styleUrls: ["./pages/main/main-common.css", "./pages/main/main.css"]
 })
 
 export class MainComponent {}
