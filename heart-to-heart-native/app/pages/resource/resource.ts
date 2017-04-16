eadeb22930e627fc465f4e2c32cbb6fc88f5f00b
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-16 00:07:12 -0600

    comment out properties that aren't being used.

--- heart-to-heart-native/app/pages/resource/resource.ts
+++ heart-to-heart-native/app/pages/resource/resource.ts
@@ -1,7 +1,7 @@
 export class Resource {
   title: string;
   description: string;
-  url: string;
-  type: string;
-  imageURL: string;
+  // url: string;
+  // type: string;
+  // imageURL: string;
 }
