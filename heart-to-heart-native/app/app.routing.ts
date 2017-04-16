e82aaf4bac99dddd937aa598e286a692c43f99b1
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-15 18:24:26 -0600

    set landing page as default path

--- heart-to-heart-native/app/app.routing.ts
+++ heart-to-heart-native/app/app.routing.ts
@@ -5,12 +5,15 @@ import { Routes } from "@angular/router";
 import { ItemsComponent } from "./item/items.component";
 import { ItemDetailComponent } from "./item/item-detail.component";
 
+import { LandingComponent } from "./pages/landing/landing.component";
+
 import { MainComponent } from "./pages/main/main.component";
 
 const routes: Routes = [
-    { path: "", redirectTo: "/main", pathMatch: "full" },
+    { path: "", redirectTo: "/landing", pathMatch: "full" },
     { path: "items", component: ItemsComponent },
     { path: "item/:id", component: ItemDetailComponent },
+    { path: "landing", component: LandingComponent },
     { path: "main", component: MainComponent }
 ];
 
