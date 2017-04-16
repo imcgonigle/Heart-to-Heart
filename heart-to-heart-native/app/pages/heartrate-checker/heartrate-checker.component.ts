bb004d453649a67ae0d42b51591423a9a2b20303
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-15 19:58:08 -0600

    create a heartrate component

---
+++ heart-to-heart-native/app/pages/heartrate-checker/heartrate-checker.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'ns-heartrate-checker',
+  templateUrl: './pages/heartrate-checker/heartrate-checker.html',
+  styleUrls: ['./pages/heartrate-checker/heartrate-checker-common.css', './pages/heartrate-checker/heartrate-checker.css']
+})
+
+export class HeartrateCheckerComponent {}
