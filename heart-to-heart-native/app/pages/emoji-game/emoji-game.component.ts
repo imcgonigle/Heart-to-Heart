dc72454c0e27acfb9a6056d465645f1655277153
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-15 21:55:48 -0600

    create emoji game page

---
+++ heart-to-heart-native/app/pages/emoji-game/emoji-game.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'ns-emoji-game',
+  templateUrl: './pages/emoji-game/emoji-game.html',
+  styleUrls: ['./pages/emoji-game/emoji-game-common.css', './pages/emoji-game/emoji-game.css']
+})
+
+export class EmojiGameComponent {}
