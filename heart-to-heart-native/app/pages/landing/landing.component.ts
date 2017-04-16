384ff52ab0d1dfdb61d342bb73f580c7940a1432
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-15 18:23:42 -0600

    create a component for the landing page

---
+++ heart-to-heart-native/app/pages/landing/landing.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: "ns-landing",
+  templateUrl: "./pages/landing/landing.component.html",
+  styleUrls: ["./pages/landing/landing-common.css", "./pages/landing/landing.css"]
+})
+
+export class LandingComponent {}
