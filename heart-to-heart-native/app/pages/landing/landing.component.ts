408beae180034d7e924c37285e148163b04dcad8
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-15 18:52:13 -0600

    create getStarted function that takes user to main page

--- heart-to-heart-native/app/pages/landing/landing.component.ts
+++ heart-to-heart-native/app/pages/landing/landing.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core'
 import { Router } from '@angular/router';
 
 @Component({
-  selector: "ns-landing",
-  templateUrl: "./pages/landing/landing.component.html",
-  styleUrls: ["./pages/landing/landing-common.css", "./pages/landing/landing.css"]
+  selector: 'ns-landing',
+  templateUrl: './pages/landing/landing.component.html',
+  styleUrls: ['./pages/landing/landing-common.css', './pages/landing/landing.css']
 })
 
 export class LandingComponent {
   constructor(private router: Router){}
-  
+
+  getStarted() {
+    this.router.navigate(['/main']);
+  }
 }
