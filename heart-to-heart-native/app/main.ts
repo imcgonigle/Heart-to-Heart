dbb7cf377df14fb4e79d1945c1093569d550f4e0
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-16 19:57:10 -0600

    import firbase

--- heart-to-heart-native/app/main.ts
+++ heart-to-heart-native/app/main.ts
@@ -2,5 +2,32 @@
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 
 import { AppModule } from "./app.module";
+import { BackendService } from "./services/backend.service";
+
+import firebase = require("nativescript-plugin-firebase");
+
+firebase.init({
+  // Optionally pass in properties for database, authentication and cloud messaging,
+  // see their respective docs.
+  persist: true,
+  storageBucket: 'gs://heart-to-heart-d364b.appspot.com',
+  onAuthStateChanged: (data: any) => {
+    console.log(JSON.stringify(data))
+    if (data.loggedIn) {
+      BackendService.token = data.user.uid;
+    }
+    else {
+      BackendService.token = "";
+    }
+  }
+  
+}).then(
+  (instance) => {
+    console.log("firebase.init done");
+  },
+  (error) => {
+    console.log("firebase.init error: " + error);
+  }
+);
 
 platformNativeScriptDynamic().bootstrapModule(AppModule);
