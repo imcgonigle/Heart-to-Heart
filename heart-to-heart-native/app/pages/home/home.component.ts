dd1c28aa502fe6d2843ad5e5c8c1f2e4d3eaba27
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-16 21:53:22 -0600

    Use firebase service to create login function

--- heart-to-heart-native/app/pages/home/home.component.ts
+++ heart-to-heart-native/app/pages/home/home.component.ts
@@ -2,21 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Color } from 'color';
 import { Page } from 'ui/page';
+import { FirebaseService } from '../../services/firebase.service';
 
 @Component({
   selector: 'ns-home',
   templateUrl: './pages/home/home.html',
-  styleUrls: ["./pages/home/home-common.css", "./pages/home/home.css"]
+  styleUrls: ["./pages/home/home-common.css", "./pages/home/home.css"],
+  providers: [FirebaseService]
 })
 
 export class HomeComponent {
   constructor(
     private page: Page, 
-    private router: Router
+    private router: Router,
+    private firebaseService: FirebaseService
   ) {}
-  
+
   ngOnInit() {
     this.page.backgroundColor = new Color("#efefef");
+  }
+
+  login() {
+    this.firebaseService.login();
   }
 
   goto(route: string) {
