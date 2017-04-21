fe6a8930a179ea9e1ca17b3eff40f785fad572d9
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-20 23:30:29 -0600

    creat goto function and delete getstarted function

--- heart-to-heart-native/app/pages/title/title.component.ts
+++ heart-to-heart-native/app/pages/title/title.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 export class TitleComponent {
   constructor(private router: Router){}
 
-  getStarted() {
-    this.router.navigate(['/main']);
+  goto(page: string) {
+    this.router.navigate([page]);
   }
 }
