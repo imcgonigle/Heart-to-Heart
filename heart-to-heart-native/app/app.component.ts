3d999b85409857c8516e8b51e8fe3edf33749edf
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2017-04-20 22:30:34 -0600

    Inject FontIcon service

--- heart-to-heart-native/app/app.component.ts
+++ heart-to-heart-native/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from "@angular/core";
+import { TNSFontIconService } from 'nativescript-ngx-fonticon';
 
 @Component({
     selector: "ns-app",
     templateUrl: "app.component.html",
 })
-export class AppComponent { }
+export class AppComponent { 
+    constructor(private fonticon: TNSFontIconService){}
+ }
